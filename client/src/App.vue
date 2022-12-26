a12f40ee76b8a64ff469dedb38c4924d4de0d1f4
JaeHyun Shin <tlswogus30@naver.com>
2022-12-26 16:11:59 +0900

    front-back 연결 이전

--- client/src/App.vue
+++ client/src/App.vue
@@ -24,19 +24,43 @@
             ></option>
           </select>
         </div>
+        <div class="col-md-2">
+          <button type="button" class="btn btn-dark">검색</button>
+        </div>
+        <div class="col-md-4">
+          <div class="row">
+            <div class="d-flex col-3 align-self-center justify-content-end">정렬기준</div>
+            <div class="col-9 align-self-center">
+              <select class="form-select" v-model="_sort">
+                <option v-for="(srt, i) in sort" :key="i" v-text="srt" :value="i"></option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex justify-content-start">
+        <el-switch v-model="value" active-text="평" inactive-text="제곱미터" />
       </div>
     </div>
   </main>
 </template>
 <script setup>
 import { ref } from "vue";
-import { division, region } from "@/localInfo.js";
+import { division, region, sort } from "@/dropDown.js";
 
 const _city = "인천시";
 const _division = ref(0);
 const _region = ref(0);
+const _sort = ref(0);
+const value = ref(true);
 
 function resetRegion() {
   _region.value = 0;
 }
 </script>
+
+<style scoped>
+button {
+  width: 80%;
+}
+</style>
